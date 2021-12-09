241f862bb34279bf7e9eb29658aa18c2fbb23304
AdelAlkhesh <splinteredsanity@outlook.com>
2021-12-09 10:31:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+}
