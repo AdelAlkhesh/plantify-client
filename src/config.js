50d322cfb79599139b1dd353437808ca7db27fa9
AdelAlkhesh <splinteredsanity@outlook.com>
2021-12-16 18:51:24 +0100

    url change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5005/api";
-// const API_URL = 'https://wdft-final.herokuapp.com/api'
+// const API_URL = "http://localhost:5005/api";
+const API_URL = 'https://wdft-final.herokuapp.com/api'
 
 export { API_URL };
